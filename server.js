01a90748574ca218ec65b42fdf946225539f17fa
aashutosh402 <chandrawanshiaashutosh@gmail.com>
2022-07-21 22:51:00 +0530

    deploying

--- server.js
+++ server.js
@@ -11,11 +11,12 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 const PORT = process.env.PORT ||5000;
 
-if(process.env.NODE_ENV == 'production'){
+
+if(process.env.NODE_ENV === 'production'){
   app.use("/",express.static(".client/build"))
 
   app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'.client/build/index.html'))
+    res.sendFile(path.resolve(__dirname,'client/build/index.html'))
   })
 }
 
